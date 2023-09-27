32fe4bcb2528aed6b5f77e8e372d5c66fe64fc5f
Allen Guan <me@nella17.tw>
2023-09-27 09:20:19 +0800

    [hw1] init shell

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # NYCU-Intro-OS-2023
+
+1. A Simple Shell
---
+++ hw1/shell.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+char** split(char* line, const char* delim, size_t* pcnt) {
+    size_t cnt = 1;
+    for (size_t i = 0, n = strlen(line); i < n; i++)
+        if (strchr(delim, line[i]) != NULL)
+            cnt++;
+    *pcnt = cnt;
+
+    char **ary = malloc(sizeof(char*) * cnt);
+    char *s = line, *saved_ptr;
+    for (int i = 0; ; i++, s = NULL) {
+        char* token = strtok_r(s, " ", &saved_ptr);
+        if (token == NULL) break;
+        ary[i] = token;
+    }
+    return ary;
+}
+
+pid_t exec(char* line) {
+    pid_t pid = fork();
+    if (pid < 0) {
+        perror("fork");
+        exit(EXIT_FAILURE);
+    }
+    if (pid == 0) {
+        size_t cnt;
+        char** argv = split(line, " ", &cnt);
+        if (cnt <= 0)
+            exit(EXIT_SUCCESS);
+        execvp(argv[0], argv);
+        perror("execve");
+        exit(EXIT_FAILURE);
+    }
+
+    return pid;
+}
+
+int main(void) {
+    char *line = NULL;
+    size_t len = 0;
+    ssize_t nread;
+
+    for (;;) {
+        printf("> ");
+        if ((nread = getline(&line, &len, stdin)) < 0)
+            break;
+        while (nread > 0 && line[nread-1] == '\n')
+            line[--nread] = '\0';
+        if (nread) {
+            pid_t pid = exec(line);
+            int wstatus;
+            waitpid(pid, &wstatus, 0);
+        }
+    }
+
+    return 0;
+}
