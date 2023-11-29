82d80e0d3bb357822349b6c254afeece42a09c2c
Allen Guan <me@nella17.tw>
2023-11-29 11:28:58 +0800

    [hw5] LRU

--- hw5/page-sim.cpp
+++ hw5/page-sim.cpp
@@ -1,8 +1,36 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdint.h>
 #include <unistd.h>
 #include <sys/time.h>
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <unordered_map>
+#include <map>
+#include <list>
+#include <chrono>
+
+struct custom_hash {
+    static uint64_t splitmix64(uint64_t x) {
+        // http://xorshift.di.unimi.it/splitmix64.c
+        x += 0x9e3779b97f4a7c15;
+        x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9;
+        x = (x ^ (x >> 27)) * 0x94d049bb133111eb;
+        return x ^ (x >> 31);
+    }
+
+    size_t operator()(uint64_t x) const {
+        static const uint64_t FIXED_RANDOM = (uint64_t)std::chrono::steady_clock::now().time_since_epoch().count();
+        return splitmix64(x + FIXED_RANDOM);
+    }
+};
+
+template<typename Key, typename Value>
+using HashTable = std::unordered_map<Key, Value, custom_hash>;
+template<typename Key, typename Value>
+using OrderMap = std::map<Key, Value>;
+template<typename Value>
+using List = std::list<Value>;
 
 double getsecond() {
     struct timeval time;
@@ -11,9 +39,93 @@ double getsecond() {
     return (double)time.tv_sec + (double)time.tv_usec / 1e6;
 }
 
-int main(void) {
+class Policy {
+  public:
+    int size, hit, miss, total;
+    const char* const name;
+    Policy(const char* p_name): name(p_name) {}
+    void init(size_t p_size) {
+        size = p_size;
+        hit = miss = total = 0;
+        _init(p_size);
+    }
+    void access(int page) {
+        total++;
+        if (_access(page))
+            hit++;
+        else
+            miss++;
+    }
+    virtual void _init(int) {}
+    virtual bool _access(int) { return false; }
+    double fault() {
+        return double(miss) / double(total);
+    }
+};
 
-    ;
+class LFU : public Policy {
+  public:
+    LFU(): Policy("LFU") {}
+    void _init(int size) override {
+
+    }
+    bool _access(int page) override {
+        return false;
+    }
+};
+
+class LRU : public Policy {
+  public:
+    List<int> list;
+    HashTable<int, List<int>::iterator> mp{};
+    LRU(): Policy("LRU") {}
+    void _init(int size) override {
+        list.clear();
+        mp.clear();
+        mp.reserve(8192);
+    }
+    bool _access(int page) override {
+        auto it = mp.find(page);
+        if (it != mp.end()) {
+            list.erase(it->second);
+            it->second = list.insert(list.end(), page);
+            return true;
+        } else {
+            if ((int)list.size() == size) {
+                mp.erase(list.front());
+                list.pop_front();
+            }
+            mp.emplace(page, list.insert(list.end(), page));
+            return false;
+        }
+    }
+};
+
+int main(int argc, char* argv[]) {
+    if (argc < 2)
+        return EXIT_FAILURE;
+
+    auto filename = argv[1];
+    std::vector<Policy*> policies{
+        new LFU,
+        new LRU
+    };
+
+    for (auto policy: policies) {
+        printf("%s policy:\n", policy->name);
+        printf("Frame\tHit\t\tMiss\t\tPage fault ratio\n");
+        auto start_time = getsecond();
+        for (int size: { 64, 128, 256, 512 }) {
+            std::ifstream in(filename);
+            int page;
+            policy->init(size);
+            while (in >> page)
+                policy->access(page);
+            printf("%d\t%d\t\t%d\t\t%.10f\n", size, policy->hit, policy->miss, policy->fault());
+        }
+        auto end_time = getsecond();
+        printf("Total elapsed time %.4f sec\n", end_time - start_time);
+    }
 
     return EXIT_SUCCESS;
 }
